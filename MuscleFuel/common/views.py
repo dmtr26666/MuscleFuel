091211669cf7e52ddaced94e8c4aaab488b82616
Dimitar Kalushkov <134792433+dmtr26666@users.noreply.github.com>
2024-11-29 22:48:57 +0200

    fix: filters bug

--- MuscleFuel/common/views.py
+++ MuscleFuel/common/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from MuscleFuel.recipes.models import Category
+
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'common/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['breakfast_id'] = Category.objects.filter(category__iexact='breakfast').first().id
+        context['lunch_id'] = Category.objects.filter(category__iexact='lunch').first().id
+        context['dinner_id'] = Category.objects.filter(category__iexact='dinner').first().id
+        context['snack_id'] = Category.objects.filter(category__iexact='snack').first().id
+
+        return context
